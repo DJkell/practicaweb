2f796c62289d4a56714d020b027131d45f40da2b
Diego Jossel <diegokell00@gmail.com>
2025-01-28 02:16:51 -0400

    Mensaje emergente

--- js-prueba/app.js
+++ js-prueba/app.js
@@ -2,12 +2,17 @@ const btnAdd = document.getElementById('add');
 const input = document.getElementById('agregar');
 const textV = document.querySelector('.list_tareas p');
 const contain = document.querySelector('.list_tareas ul');
+const openAdd = document.getElementById('addTarea')
+const popCuadro = document.querySelector('.pop-cuadro')
+const btnExit = document.querySelector('.btn-x');
 
+openAdd.addEventListener('click', ()=>{
+    popCuadro.classList.toggle('show');
+});
 
-
-
-
-
+btnExit.addEventListener('click', ()=>{
+    popCuadro.classList.toggle('show');
+})
 
 btnAdd.addEventListener('click', (e)=>{
     e.preventDefault();
